d2614b280a8d4956b321f20389cf177b149734de
AlanMartinez <alann.m-@hotmail.com>
2015-01-13 10:04:53 -0300

    agregue archivos

--- reconocedor.py
+++ reconocedor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import scipy.io as sio
 import numpy as np
 import pygame.font, pygame.event, pygame.draw
@@ -16,12 +19,12 @@ pantalla = None
 def dividirDatos(X, y):
     """Divide la muestra de datos en un conjunto de entrenamiento (80%) y uno de prueba (20%)"""
     
-    tamaño1 = X.shape[0] * 0.8
-    tamaño2 = X.shape[0] * 0.2
-    Xentrenamiento = np.zeros((tamaño1,X.shape[1]))
-    Xprueba = np.zeros((tamaño2,X.shape[1]))
-    yentrenamiento = np.zeros((tamaño1,1))
-    yprueba = np.zeros((tamaño2,1))
+    tamanio1 = X.shape[0] * 0.8
+    tamanio2 = X.shape[0] * 0.2
+    Xentrenamiento = np.zeros((tamanio1,X.shape[1]))
+    Xprueba = np.zeros((tamanio2,X.shape[1]))
+    yentrenamiento = np.zeros((tamanio1,1))
+    yprueba = np.zeros((tamanio2,1))
     for i, v in enumerate(np.random.permutation(len(y))):
         #print(i, y[v], len(X[v]))
         
@@ -29,8 +32,8 @@ def dividirDatos(X, y):
             Xentrenamiento[i] = X[v]
             yentrenamiento[i] = y[v]
         except:
-            Xprueba[i-tamaño1] = X[v]
-            yprueba[i-tamaño1] = y[v]
+            Xprueba[i-tamanio1] = X[v]
+            yprueba[i-tamanio1] = y[v]
     return Xentrenamiento, Xprueba, yentrenamiento, yprueba
     
 def calcularImagen(fondo, pantalla, Theta1, Theta2, ancho_de_linea):
@@ -255,3 +258,202 @@ def probarTeclas(datos):
 
     datos = (evento, fondo, color, ancho_de_linea, continuar)
     return datos
+
+
+def dibujarEstadisticas(ancho_de_linea, valor, probabilidad):
+    """ Muestra las estadisticas actuales """
+    
+    fuente = pygame.font.SysFont("Verdana", 50)
+    estadisticas = "Estimate: %s    P: %s" % (valor, probabilidad)
+    return fuente.render(estadisticas+"%", 1, ((255, 255, 255)))
+
+
+def dibujarEstadisticasPequenia(ancho_de_linea, valor, probabilidad):
+
+    fuente = pygame.font.SysFont("Verdana", 25)
+    estadisticas = "Second estimate: %s (%s" % (valor, probabilidad)
+    return fuente.render(estadisticas+"%)", 1, ((235, 235, 235)))
+
+def dibujarPixelated(A, pantalla):  
+    """dibujar 30x30 imagen para entrada""" 
+  
+    A = A.ravel()
+    A = (255-A*255).transpose()
+    tamanio = 25
+    for x in range(tamanio):
+        for y in range(tamanio):
+            z=x*30+y
+            c = int(A[z])
+            pygame.draw.rect(pantalla,(c,c,c),(x*11+385,15+y*11,11,11))
+
+
+
+def dibujarEstadisticas(pantalla):  
+    """Dibuja las estadisticas en la pantalla"""
+
+    contenido_mat = sio.loadmat('newX.mat')
+    Xs = contenido_mat['X']
+    contenido_mat = sio.loadmat('newy.mat')
+    ys = contenido_mat['y']
+    Xtrain, Xtest, ytrain, ytest = splitData(Xs,ys)
+    contenido_mat = sio.loadmat('scaledTheta.mat')
+    acc = float(contenido_mat['acc'])
+    y = ys.ravel().tolist()
+
+    myFont = pygame.font.SysFont("Verdana", 24)
+    myFont2 = pygame.font.SysFont("Verdana", 18)
+    myFont3 = pygame.font.SysFont("Verdana", 16)
+    pygame.draw.rect(pantalla,(255,255,255),(370,0,730,360))
+    pantalla.blit(myFont.render("Muestras: %d" % (Xs.shape[0]), 1, ((0, 0, 0))), (400, 30))
+    pantalla.blit(myFont.render("Precision: %s" % str(acc)+"%", 1, ((0, 0, 0))), (400, 60))
+    pantalla.blit(myFont3.render("DISTRIBUCION DE LA MUESTRA:", 1, ((0, 0, 0))), (400, 100))
+    pantalla.blit(myFont2.render("Total 0 = %s" % (y.count(0)), 1, ((0, 0, 0))), (400, 120))
+    for i in range(9):
+        pantalla.blit(myFont2.render("Total %s = %s" % (i+1, y.count(i+1)), 1, ((0, 0, 0))), (400, 140+i*20))
+
+
+def inicializacionAleatoria(i, epsilon=0.12):
+    """Para la ruptura de la simetria inicializar thetas al azar"""
+    return np.random.rand(i,1)*2*epsilon-epsilon
+
+
+def gradienteSigmoide(z):
+    """Gradient of sigmoid function"""
+    return np.multiply(sigmoid(z),(1-sigmoid(z)));
+
+
+def unirVectores(v1, v2):
+    return np.append(np.ravel(v1), np.ravel(v2))
+
+
+def backProp(p, numero_de_entrada, numero_oculto, numero_de_etiquetas, X, valor_de_y, l=0.2):
+    """Backpropagation algorithm of neural network"""
+    
+    Theta1 = np.reshape(p[:numero_oculto*(numero_de_entrada+1)], (numero_oculto,-1))
+    Theta2 = np.reshape(p[numero_oculto*(numero_de_entrada+1):], (numero_de_etiquetas,-1))
+    m = len(X)
+    delta1 = 0
+    delta2 = 0
+    for t in range(m):
+
+        a1 = np.matrix(np.append([1],X[t],axis=1)).transpose()
+        z2 = Theta1*a1
+        a2 = np.append(np.ones(shape=(1,z2.shape[1])), sigmoid(z2),axis=0)
+        z3 = Theta2*a2
+        a3 = sigmoid(z3)
+        w = np.zeros((numero_de_etiquetas,1))
+        w[int(valor_de_y[t])-1] = 1
+        d3 = (a3-w)
+        d2 = np.multiply(Theta2[:,1:].transpose()*d3, gradienteSigmoide(z2))
+        delta1 += d2*a1.transpose()
+        delta2 += d3*a2.transpose()
+        
+    
+    Theta1_grad = (1/m)*delta1 + (l/m)*np.append(np.zeros(shape=(Theta1.shape[0],1)), Theta1[:,1:], axis=1);
+    Theta2_grad = (1/m)*delta2 + (l/m)*np.append(np.zeros(shape=(Theta2.shape[0],1)), Theta2[:,1:], axis=1);
+   
+    return unirVectores(Theta1_grad, Theta2_grad)
+
+def J(theta, numero_de_entrada, numero_oculto, numero_de_etiquetas,X, valor_de_y, l=0.2):
+    """Cost funtion"""
+    
+    Theta1 = np.reshape(theta[:numero_oculto*(numero_de_entrada+1)], (numero_oculto,-1))
+    Theta2 = np.reshape(theta[numero_oculto*(numero_de_entrada+1):], (numero_de_etiquetas,-1))
+    m = len(X)
+    X = np.append(np.ones(shape=(X.shape[0],1)),X,axis=1)
+    J = 0
+    for i in range(m):
+        x = np.matrix(X[i])
+        w = np.zeros((10,1))
+        w[int(valor_de_y[i])-1] = 1
+        hx = sigmoid(Theta2*np.append([[1]], sigmoid(Theta1*x.transpose()), axis=0))
+        J += sum(-w.transpose()*np.log(hx)-(1-w).transpose()*np.log(1-hx))
+    J = J/m
+    J += (l/(2*m))*(sum(sum(Theta1[:,1:]**2)) + sum(sum(Theta2[:,1:]**2)))    
+    return float(J)
+
+def calculateGrad(p):
+    """Metodo Backpropagation para optimizar"""
+    
+    return backProp(p, 900, 25, 10, Xtrain,ytrain)
+    
+def calculateJ(p):
+    """Metodo de costo para optimizar"""
+    return J(p, 900, 25, 10, Xtrain, ytrain)
+
+def callback(p):
+    """Actualiza la GUI mientras se entrena la red neuronal"""
+    global total
+    global pantalla
+    pygame.event.get()
+    total += 1
+    fuente = pygame.font.SysFont("Verdana", 110)
+    puntos= []
+    if total >= 9:
+        total = 1
+        pygame.draw.rect(pantalla,(255,255,255),(355,220,400,250))
+        puntos = []
+    for i in range(total):
+        puntos.append(".")
+    pantalla.blit(fuente.render("".join(puntos), 1, ((150, 150, 150))), (355, 140))
+    pygame.display.flip()
+
+
+def main():
+    global pantalla
+    pygame.init()
+    pantalla = pygame.display.set_mode((730, 450))
+    pygame.display.set_caption("Reconocimiento de caracteres")
+    
+    fondo = pygame.Surface((360,360))
+    fondo.fill((255, 255, 255))
+    fondo2 = pygame.Surface((360,360))
+    fondo2.fill((255, 255, 255))
+    
+    reloj = pygame.time.Clock()
+    sigue = True
+    linea_de_inicio = (0, 0)
+    color_de_dibujo = (255, 0, 0)
+    ancho_de_linea = 15
+    
+    inputTheta = sio.loadmat('scaledTheta.mat')
+    theta = inputTheta['t']
+    numero_oculto = 25
+    numero_de_entrada = 900
+    numero_de_etiquetas = 10
+
+    Theta1 = np.reshape(theta[:numero_oculto*(numero_de_entrada+1)], (numero_oculto,-1))
+    Theta2 = np.reshape(theta[numero_oculto*(numero_de_entrada+1):], (numero_de_etiquetas,-1))
+
+    pygame.display.update()
+    imagen = None
+            
+    while sigue:
+        
+        reloj.tick(30)
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                sigue = False
+            elif evento.type == pygame.MOUSEMOTION:
+                linea_de_fin = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed() == (1, 0, 0):
+                    pygame.draw.line(fondo, color_de_dibujo, linea_de_inicio, linea_de_fin, ancho_de_linea)
+                linea_de_inicio = linea_de_fin
+            elif evento.type == pygame.MOUSEBUTTONUP:
+                pantalla.fill((0, 0, 0))
+                pantalla.blit(fondo2, (370, 0))
+                #w = threading.Thread(name='worker', target=worker)
+                imagen = calcularImagen(fondo, pantalla, Theta1, Theta2, ancho_de_linea)
+
+            elif evento.type == pygame.KEYDOWN:
+                misDatos = (evento, fondo, color_de_dibujo, ancho_de_linea, sigue, pantalla, imagen)
+                misDatos = checkKeys(misDatos)
+                (evento, fondo, color_de_dibujo, ancho_de_linea, sigue) = misDatos
+        
+        
+        pantalla.blit(fondo, (0, 0))
+        pygame.display.flip()
+
+        
+if __name__ == "__main__":
+    main()

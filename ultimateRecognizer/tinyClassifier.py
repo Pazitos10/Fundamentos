193348da3b5dc30716bb34edf92725cfaef29cb8
Pazitos10 <pazosbruno@hotmail.com>
2015-03-11 13:40:31 -0300

    updating tests module

--- ultimateRecognizer/tinyClassifier.py
+++ ultimateRecognizer/tinyClassifier.py
@@ -5,64 +5,61 @@
 from __future__ import division
 from sklearn import metrics, cross_validation
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import StandardScaler 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.dummy import DummyClassifier 
+from sklearn.dummy import DummyClassifier
+import numpy as np
 
-std_scaler = StandardScaler() 
 
 
 
 class TinyClassifier(object):
     def __init__(self):
-        super(TinyClassifier, self).__init__()
-        # self.expected = None
-        # self.X = []
-        self.predicted = None
+        super(TinyClassifier, self).__init__() 
 
     def extra_trees_clf(self):
-        return ExtraTreesClassifier( n_estimators=4,
-                                    criterion='gini',
-                                    max_features=0.2,
-                                    n_jobs=4,
+        #Defaults => n_estimators=4, criterion='gini',max_features=0.2,n_jobs=4,random_state=1
+        return ExtraTreesClassifier(max_features='auto',
+                                    n_jobs=-1,
                                     random_state=1)
 
-     
     def dummy_clf(self): 
-        return DummyClassifier(strategy='most_frequent',random_state=0)
-     
-     
+        return DummyClassifier(strategy='most_frequent',random_state=np.random.randint(0,9))
+          
     def support_vector_clf(self):
-        return SVC(kernel='linear')
+        #default kernel= 'linear'
+        return SVC(kernel='poly')
 
     def k_neighbors_clf(self):
-        return KNeighborsClassifier(n_neighbors=15)
-
+        return KNeighborsClassifier(n_neighbors=9, weights='distance')
 
     def train(self, clf, X, y): #entrenar
         clf.fit(X,y)
     
     def predict(self, clf, X): #predecir/probar/testear 
-        self.predicted = clf.predict(X)
-        return self.predicted
+        return clf.predict(X)
     
     def splitData(self, X, y):
-#        global std_scaler
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#        X_train = std_scaler.fit_transform(X_train)
-#        X_test = std_scaler.transform(X_test) 
-        return X_train, X_test, y_train, y_test
+        return train_test_split(X, y, test_size=0.2)
 
     def getGlobalAccuracy(self, clf, y_test, y_predicted):
         result = cross_validation.cross_val_score(clf,y_test, y_predicted, cv=5, scoring='accuracy')
         result = result.mean()
         result *= 100.0
-        print 'accuracy score: %0.2f' % result
+        saved_results = open('results.txt', 'a+')
+        line = '\n\n %s \t| Accuracy score: %0.2f %% \n' % (clf.__class__.__name__,result)
+        line += '-'*100+'\n'
+        saved_results.write(line)
+        saved_results.close()
         return result
 
-    def print_metrics(self,clf, expected, predicted): #salida por consola
-        print("Classification report for classifier %s:\n%s\n"
-              % (clf, metrics.classification_report(expected, predicted)))
-        print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+    def save_metrics(self,clf, expected, predicted): #salida por archivo
+        results = open('results.txt', 'a+')
+        matrix = metrics.confusion_matrix(expected, predicted)
+        report = metrics.classification_report(expected, predicted)
+        line = "Classification report for classifier %s:\n%s\n" % (clf,report)
+        line += "Confusion matrix:\n%s" % matrix
+        results.write(line)
+        results.close()
+        return matrix

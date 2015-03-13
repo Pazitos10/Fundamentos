e89dab22cb458c75e02ca048b7fac7a598ce4eae
bruno <bruno@bruno-VirtualBox.(none)>
2015-03-13 19:23:59 -0300

    upgrading files(?

--- ultimateRecognizer/cgi-bin/procesador_datos.py
+++ ultimateRecognizer/cgi-bin/procesador_datos.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 # Import modules for CGI handling 
+import os
+from PIL import Image
+from StringIO import StringIO
+from base64 import decodestring
 import cgi, cgitb 
-
+import re
+import numpy as np
+width = 200
+height = 200
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
+
 # Get data from fields
 datos = form.getvalue('matriz_canvas')
+
+img = Image(datos)
+img = img.getdata()
+img = img.resize((8,8))
+img = map(list, img)
+img = np.array(img)
+s = img.shape[0] * img.shape[1]
+img_wide = img.reshape(1, s)
+
+
+#import ipdb
+#ipdb.set_trace()
 
 print "Content-type:text/html\r\n\r\n"
 print '<html>'
@@ -14,5 +34,5 @@ print '<head>'
 print '<title>Hello Word - First CGI Program</title>'
 print '</head>'
 print '<body>'
-print '%s' % datos
+print 'tipo: %s - valor: %s' % (str(type(img_wide[0] )), len(img_wide[0]) )
 print '</body></html>'
